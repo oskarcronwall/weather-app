3067baa88bab50b51202aedd2ba3cc794c8ea559
Oskar Cronwall <Oskar.Cronwall@hiq.se>
2016-04-04 16:01:34 +0200

    Api for locations implemented on server

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-
+var fs = require('fs');
+var csvParse = require('csv-parse');
 var app = express();
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -14,9 +16,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/bower', express.static(path.join(__dirname, 'bower_components')));
 app.use(express.static(path.join(__dirname, 'app')));
 
-// app.get('/api', function(res,res){
-//   res.send('api');
-// })
+app.get('/locations', function(res, res){
+  fs.readFile('data/coords.csv', function(fileError, data){
+    if(fileError){
+      return console.log(fileError);
+    }
+    csvParse(data, {columns: ['location','lat','lon']}, function(parseError, output){
+      if(parseError){
+        return console.log(parseError);
+      }
+      res.send(output);
+    })
+  })
+})
 
 app.get('*', function(req, res){
   res.sendFile(path.join(__dirname, '/app/index.html'));

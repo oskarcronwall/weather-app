4bd135e95043ee0daf2511646509a987b7a4ddf7
Oskar Cronwall <Oskar.Cronwall@hiq.se>
2016-04-06 01:25:01 +0200

    weatherModel now handles max and min temperatures

--- app/weather/weatherModel.js
+++ app/weather/weatherModel.js
@@ -16,7 +16,14 @@ angular.module('weatherApp.weather')
 					dates[dateString].data.push(obj);
 				}
 			})
+			
+			for (var date in dates){
+				dates[date]['tempMax'] = maxValue('t', dates[date]['data']);
+				dates[date]['tempMin'] = minValue('t', dates[date]['data']);
+			}
+
 			data['dates'] = dates;
+			console.log(data);
 			return data;
 		})
 	}
@@ -24,6 +31,27 @@ angular.module('weatherApp.weather')
 	function weekDayInSwedish(date){
 		var dateObj = new Date(date);
 		return dateObj.toLocaleDateString('sv-SE', {weekday:'long'});
+	}
+	function filterOnKey(key, arr){
+		var filter = arr.map(function(obj){
+			return obj[key];
+		})
+		return filter;
+	}
+	function maxValue(key, arr){
+		return Math.max(...filterOnKey(key, arr));
+	}
+
+	function minValue(key,arr){
+		return Math.min(...filterOnKey(key, arr));
+	}
+
+	function average(key, arr){
+		var sum = arr.reduce(function(a,b){
+			return a[key] + b[key]
+		});
+		return sum / arr.length;
+
 	}
 
 	return {

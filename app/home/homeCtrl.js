9bf316dbb0ce917f9f4e3374dc306710e5ee971b
Oskar Cronwall <Oskar.Cronwall@hiq.se>
2016-04-05 12:59:46 +0200

    Search implemented

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -1,5 +1,6 @@
 angular.module('weatherApp.home')
 
 .controller('HomeCtrl', ['$scope', 'locationResource', function($scope, locationResource){
+	$scope.selected = undefined;
 	$scope.locations = locationResource.query();
 }])

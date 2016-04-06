3ca2ad187c9c426c69f5f4d56196b386a03541f2
Oskar Cronwall <Oskar.Cronwall@hiq.se>
2016-04-06 15:18:24 +0200

    weatherModel now calculates wind speed and air pressure

--- app/weather/weatherModel.js
+++ app/weather/weatherModel.js
@@ -9,52 +9,72 @@ angular.module('weatherApp.weather')
 			data.timeseries.forEach(function(obj){
 				var dateString = obj.validTime.slice(0, 10);
 				if(!dates[dateString]){
-					var label = weekDayInSwedish(dateString);
-					dates[dateString] = {label: label, data:[obj]};
+					dates[dateString] = {
+						label: weekDayInSwedish(dateString), 
+						data:[obj]
+					};
 				}
 				else{
 					dates[dateString].data.push(obj);
-				}
+				};
 			})
 			
 			for (var date in dates){
 				dates[date]['tempMax'] = maxValue('t', dates[date]['data']);
 				dates[date]['tempMin'] = minValue('t', dates[date]['data']);
+				dates[date]['windSpeedMedian'] = median('ws', dates[date]['data']);
+				dates[date]['windSpeedAverage'] = average('ws', dates[date]['data']);
+				dates[date]['airPressureAverage'] = average('msl', dates[date]['data']);
 			}
 
 			data['dates'] = dates;
 			console.log(data);
 			return data;
 		})
-	}
+	};
 
 	function weekDayInSwedish(date){
 		var dateObj = new Date(date);
 		return dateObj.toLocaleDateString('sv-SE', {weekday:'long'});
-	}
+	};
 	function filterOnKey(key, arr){
-		var filter = arr.map(function(obj){
+		return arr.map(function(obj){
 			return obj[key];
-		})
-		return filter;
-	}
+		});
+	};
 	function maxValue(key, arr){
-		return Math.max(...filterOnKey(key, arr));
-	}
+		var f = filterOnKey(key, arr);
+		return Math.max(...f);
+	};
 
 	function minValue(key,arr){
-		return Math.min(...filterOnKey(key, arr));
-	}
+		var f = filterOnKey(key, arr)
+		return Math.min(...f);
+	};
 
 	function average(key, arr){
-		var sum = arr.reduce(function(a,b){
-			return a[key] + b[key]
-		});
+		var f = filterOnKey(key, arr);
+		var sum = f.reduce(function(a, b){
+			return a + b;
+		})
 		return sum / arr.length;
+	};
 
-	}
+	function median(key, arr){
+		var f = filterOnKey(key, arr);
+		var m = f.sort(function(a, b) {
+	        return a - b;
+	    });
+
+	    var middle = Math.floor((m.length - 1) / 2);
+	    if (m.length % 2) {
+	        return m[middle];
+	    } else {
+	        return (m[middle] + m[middle + 1]) / 2.0;
+	    };
+	};
 
 	return {
 			getWeatherReport:getWeatherReport
-		}
+		};
 }])

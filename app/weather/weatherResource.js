27c85f152e16d8a679fc567db9c8e3ad535beb26
Oskar Cronwall <Oskar.Cronwall@hiq.se>
2016-04-07 12:41:40 +0200

    Changed to version 2 of SMHI PMP API

--- app/weather/weatherResource.js
+++ app/weather/weatherResource.js
@@ -1,7 +1,7 @@
 angular.module('weatherApp.weather')
 .factory('weatherResource', ['$resource', function($resource){
 	return $resource(
-		'http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/:lat/lon/:lon/data.json',
+		'http://opendata-download-metfcst.smhi.se/api/category/pmp2g/version/1/geopoint/lat/:lat/lon/:lon/data.json',
 		{
 			lat:"@lat",
 			lon:"@lon"

c8e30fdb8e0d31d6bba4c8431411a0537ed73b6e
Oskar Cronwall <Oskar.Cronwall@hiq.se>
2016-04-07 14:12:31 +0200

    Only show search error message if user has typed something that does not match

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -10,6 +10,11 @@ angular.module('weatherApp.home')
 	$scope.locations = locationModel.getLocations();
 	$scope.center = mapModel.init();
 
+	$scope.$watch($scope.selected, function(val){
+		if (typeof val !== 'undefined' && val.length == 0)
+			$scope.noResults = false;
+	})
+
 	$scope.selectLocation = function($item, $label){
 		$scope.locationName = $label;
 		$scope.weatherReport = weatherModel.getWeatherReport($item)
